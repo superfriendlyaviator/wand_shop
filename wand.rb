3b8e280f5e9b36463674a424fcf59e4558895d83
superfriendlyaviator <nebula9485@gmail.com>
2013-08-26 22:45:03 -0400

    Cleaned up description.

--- wand.rb
+++ wand.rb
@@ -1,20 +1,15 @@
 class Wand 
-  attr_accessor :name, :cost, :description, :power_level
+  attr_accessor :name, :cost, :description
 
-  def initialize(name, cost, power_level)
+  def initialize(name, cost, description)
     @name = name 
-    @cost = cost 
-    @description = description 
-    @power_level = power_level
+    @cost = cost  
+    @description = description
   end
+  
+  #def attack
+  #end
 
-  def describe
-    puts "Enter your description of this wand: "
-    @description = gets.chomp 
-  end 
-
-  def description
-    puts @description
-  end
 end
+
 
