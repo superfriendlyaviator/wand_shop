4bb58f6ea5952231360cb5492475662ecc5dd68d
superfriendlyaviator <nebula9485@gmail.com>
2013-08-28 23:53:04 -0400

    Now you can sell wands.

--- wandshop.rb
+++ wandshop.rb
@@ -7,14 +7,14 @@ class Wandshop
   
   def initialize
     @player_gold = 100
-    @player_wands = []
+    @player_wands = [KnownItems::TOOTHPICK_WAND, KnownItems::SPOON_WAND, KnownItems::PAPER_CUT_OUT]
     @wandmenu = KnownItems.items_by_name
     @wand_options = "Welcome to the wand shop. Please select an option.
       Press 1 to see all wands. 
       Press 2 to buy a new wand. 
       Press 3 to sell your old wand.
       Press 4 to see your wands.
-      Press 5 to see your gold. 
+      Press 5 to see your gold.
       Press 6 to leave the wand shop." 
   end
 
@@ -28,7 +28,6 @@ class Wandshop
     puts "Please select a wand from the menu." 
     user_wants = gets.chomp.downcase
     wand = wandmenu[user_wants]
-
     if wand
       if player_gold > wand.cost
         puts wand.description
@@ -45,23 +44,35 @@ class Wandshop
     end 
   end
 
-  def sell_wands 
+  def sell_wands
+    if player_wands.empty?
+      puts "You don't have any wands to sell!"
+      return
+    end
+    
     puts "So you want to sell your old wands? Great! You have these wands now: "
-    puts player_wands 
-    puts "We buy all wands for 10 gold.Tell me which one you want to chuck." 
-    wand_sell = gets.chomp.downcase
-    player_wands.delete(wand_sell)
-    self.player_gold += 10
+    player_wands.each_with_index{|wand, index| puts "#{index+1} - #{wand.name}"}
+    puts "We buy all wands for half their value. Tell me which one you want to chuck:" 
+    wand_sell_index = gets.chomp.to_i
+
+    if wand_sell_index > 0 && wand_sell_index <= player_wands.length
+      sold_wand = player_wands.delete_at(wand_sell_index-1)
+      self.player_gold += sold_wand.resale_value
+      puts "Thanks for selling the #{sold_wand.name} for #{sold_wand.resale_value} gold! You now have #{player_gold} gold!"
+    else
+      puts "If you are just going to play around, I'm going to have to ask you to leave."    
+    end
   end
 
-  def show_wands 
-    puts player_wands.collect{|wand| puts wand}
+  def show_wands
+    puts "You currently own:"
+    player_wands.each{|wand| puts wand.name}
   end
   
   def show_gold 
     puts "You have #{player_gold} gold. Spend it wisely." 
   end 
-
+ 
   def leave 
     puts "Thank you for your purchase. Good luck fighting those shadow monsters!"
     exit
